48ab105426d4126e0dd012c61389a9c78365b7cd
Khaled Walead Abu Eriban <91320064+khaled50k@users.noreply.github.com>
2023-09-05 13:56:32 +0300

    updating

--- src/validation/prescriptionValidation.js
+++ src/validation/prescriptionValidation.js
@@ -1,25 +1,25 @@
 const Joi = require("joi");
 
 const prescriptionSchema = Joi.object({
-  patient: Joi.string().required(),
-  dentist: Joi.string().required(),
+  patient: Joi.string().required().min(1).max(100), // Adjust min and max lengths as needed
+  dentist: Joi.string().required().min(1).max(100), // Adjust min and max lengths as needed
   prescriptionDate: Joi.date().required(),
-  chiefComplaint: Joi.string(),
+  chiefComplaint: Joi.string().max(255), // Adjust max length as needed
   medicines: Joi.array().items(
     Joi.object({
-      medicineName: Joi.string().required(),
-      medicineType: Joi.string(),
-      instructions: Joi.string(),
-      days: Joi.number(),
+      medicineName: Joi.string().required().min(1).max(100), // Adjust min and max lengths as needed
+      medicineType: Joi.string().max(50), // Adjust max length as needed
+      instructions: Joi.string().max(255), // Adjust max length as needed
+      days: Joi.number().integer().min(1), // Adjust min value as needed
     })
   ),
   diagnoses: Joi.array().items(
     Joi.object({
-      diagnosis: Joi.string(),
-      diagnosisInstructions: Joi.string(),
+      diagnosis: Joi.string().max(255), // Adjust max length as needed
+      diagnosisInstructions: Joi.string().max(255), // Adjust max length as needed
     })
   ),
-  notes: Joi.string(),
+  notes: Joi.string().max(1000), // Adjust max length as needed
 });
 
 module.exports = {
